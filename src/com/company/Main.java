dde5919fb9b2de2726acf18ec34ae8fc5b488418
Eugeniusz W <eugwas@gmail.com>
2021-10-27 10:33:29 +0200

    Scanner was added

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,20 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println(LeapYear.isLeapYear(2000));
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the number: ");
+        boolean hasNextInt = scanner.hasNextInt();
+        if (hasNextInt) {
+            int number = scanner.nextInt();
+            System.out.println("Is leap year = " + LeapYear.isLeapYear(number));
+        } else {
+            System.out.println("Invalid number");
+        }
+        scanner.nextLine();
+        scanner.close();
     }
 }
